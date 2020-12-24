7fa28025bf25a3bc14cf9491ee361b4af7356557
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2020-12-24 13:42:50 -0300

    column year from string to enum

--- database/migrations/1601818098259_questions_schema.js
+++ database/migrations/1601818098259_questions_schema.js
@@ -85,7 +85,32 @@ class QuestionsSchema extends Schema {
       // Qual vestibular essa questão apareceu
       table.string('exam').notNullable();
       // Em qual ano ?
-      table.integer('year').notNullable();
+      table
+        .enum('year', [
+          2000,
+          2001,
+          2002,
+          2003,
+          2004,
+          2005,
+          2006,
+          2007,
+          2008,
+          2009,
+          2010,
+          2011,
+          2012,
+          2013,
+          2014,
+          2015,
+          2016,
+          2017,
+          2018,
+          2019,
+          2020,
+          2021,
+        ])
+        .notNullable();
       table
         .enum('status', ['aprovada', 'reprovada', 'pendente'])
         .default('pendente');
---
+++ database/migrations/1608827313596_exam_schema.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class ExamSchema extends Schema {
+  up() {
+    this.create('exams', (table) => {
+      table.increments();
+      table.string('exam');
+      table.enum('location', [
+        'AC',
+        'AL',
+        'AP',
+        'AM',
+        'BA',
+        'CE',
+        'ES',
+        'GO',
+        'MA',
+        'MT',
+        'MS',
+        'MG',
+        'PA',
+        'PB',
+        'PR',
+        'PE',
+        'PI',
+        'RJ',
+        'RN',
+        'RS',
+        'RO',
+        'RR',
+        'SC',
+        'SP',
+        'SE',
+        'TO',
+        'DF',
+        'BR',
+      ]);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('exams');
+  }
+}
+
+module.exports = ExamSchema;
