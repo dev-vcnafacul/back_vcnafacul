959f7bcfe024f3e124eed42d3813ce8cc8b9478a
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2021-01-02 16:18:02 -0300

    Register n questions

--- app/Controllers/Http/QuestionController.js
+++ app/Controllers/Http/QuestionController.js
@@ -10,6 +10,8 @@ const Answer = use('App/Models/Answer');
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Exam = use('App/Models/Exam');
 
+const { validate } = use('Validator');
+
 /**
   @typedef {import('@adonisjs/framework/src/Request')} Request
   @typedef {import('@adonisjs/framework/src/View')} View
@@ -38,69 +40,86 @@ class QuestionController {
   // Verificar quem tem atribuição de cadastro. Para isso Olhar na Tabela de Permissões.
 
   async store({ request, auth, response }) {
-    if (!this.PermissionVerification(auth, 'cadastroQuestao')) {
-      response.status(404);
-      return { error: 'Not authorization' };
+    const Pm = await Permission.query().where('user_id', auth.user.id).fetch();
+    const perm = Pm.toJSON();
+    if (perm.cadastroQuestao === 0) {
+      return response.status(200).json({ error: 'Not Authorization' });
     }
-    const {
-      question,
-      enemArea,
-      year,
-      examId,
-      frente,
-      subjects,
-      answerCorrect,
-      answerWrong,
-    } = request.only([
-      'question',
-      'enemArea',
-      'year',
-      'examId',
-      'frente',
-      'subjects',
-      'answerCorrect',
-      'answerWrong',
-    ]);
 
-    const newquest = {
-      user_id: auth.user.id,
-      question,
-      enemArea,
-      subjects,
-      year,
-      examId,
-      frente,
+    let qtError = [];
+    let qtSucess = [];
+
+    const MyQuestions = request.all();
+    let arraQuestion = [];
+
+    const count = Object.keys(MyQuestions).length;
+
+    for (let i = 0; i < count; i += 1) {
+      arraQuestion = arraQuestion.concat(MyQuestions[i]);
+    }
+
+    const rulesQuestion = {
+      user_id: 'required|integer',
+      question: 'required|string',
+      enemArea: 'required|string',
+      subjects: 'required|string',
+      year: 'required|integer',
+      examId: 'required|integer',
+      frente: 'required|string',
     };
 
-    const exam = await Exam.findBy('id', examId);
+    await Promise.all(
+      arraQuestion.map(async (elem) => {
+        const qt = {
+          user_id: auth.user.id,
+          question: elem.question,
+          enemArea: elem.enemArea,
+          subjects: elem.subjects,
+          year: elem.year,
+          examId: elem.examId,
+          frente: elem.frente,
+        };
 
-    if (!exam) {
-      response.status(404);
-      return { error: 'Vestibular não cadastrado' };
-    }
+        await Exam.findByOrFail('id', elem.examId);
 
-    try {
-      const quest = await Question.create(newquest);
+        const validation = await validate(qt, rulesQuestion);
 
-      await Answer.create({
-        question_id: quest.id,
-        answer: answerCorrect,
-        correct: true,
-      });
-      await Promise.all(
-        answerWrong.map(async (elem) => {
-          await Answer.create({
-            question_id: quest.id,
-            answer: elem,
+        if (
+          validation.fails() ||
+          elem.answerCorrect === '' ||
+          elem.answerWrong.length !== 4
+        ) {
+          qtError = qtError.concat({
+            error: 'validation fails',
+            question: elem.question,
           });
-        })
-      );
-      response.status(200);
-      return quest;
-    } catch (error) {
-      response.status(400);
-      return { error };
-    }
+        } else {
+          try {
+            const myQuest = await Question.create(qt);
+            await Answer.create({
+              question_id: myQuest.id,
+              answer: elem.answerCorrect,
+              correct: true,
+            });
+            await Promise.all(
+              elem.answerWrong.map(async (wrong) => {
+                await Answer.create({
+                  question_id: myQuest.id,
+                  answer: wrong,
+                });
+              })
+            );
+            qtSucess = qtSucess.concat(qt.question);
+          } catch (error) {
+            qtError = qtError.concat({
+              error,
+              question: qt.Question,
+            });
+          }
+        }
+      })
+    );
+    return response.status(200).json({ qtError, qtSucess });
   }
 
   async show({ auth, request, response }) {
